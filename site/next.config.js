99a1d2de8541023d7c995cedd41e8ea393c4d04d
Makaveli96bg <116718037+Makaveli96bg@users.noreply.github.com>
2025-01-28 12:49:22 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,11 +4,11 @@ const nextConfig = {
   env: {
     name: "Makaveli96bg's Kasm Registry",
     description: "A custom registry for Kasm workspaces.",
-    icon: "https://github.com/Makaveli96bg.png",  // You can change this to a custom logo URL
-    listUrl: "https://makaveli96bg.github.io/kasm-registry/",  // This is your GitHub Pages link
-    contactUrl: "https://github.com/Makaveli96bg/kasm-registry/issues",
+    icon: "https://github.com/kasm-registry2/Makaveli96bg.png",  // You can change this to a custom logo URL
+    listUrl: "https://makaveli96bg.github.io/kasm-registry2/",  // This is your GitHub Pages link
+    contactUrl: "https://github.com/Makaveli96bg/kasm-registry2/issues",
   },
-  basePath: "/kasm-registry/1.0",
+  basePath: "/kasm-registry2/1.0",
 };
 
 module.exports = nextConfig;
